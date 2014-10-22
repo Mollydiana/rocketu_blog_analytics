fbcca53a4a5eed41796342fba86877d55c32e8e9
Molly <Mollydiana@gmail.com>
2014-10-21 17:09:48 -0700

    moved static folder

--- rocketu_blog_analytics/settings.py
+++ rocketu_blog_analytics/settings.py
@@ -104,11 +104,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'rocketu_blog_analytics/static')
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "static", *MEDIA_URL.strip("/").split("/"))
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "rocketu_blog_analytics/static", *MEDIA_URL.strip("/").split("/"))
 
 try:
     from local_settings import *
